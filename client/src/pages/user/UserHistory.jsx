1315bee1622006221de9ad5d01b64a9a0d01f639
Abhishek <abhishek404singh@gmail.com>
2022-01-13 16:19:31 +0530

    created user navbar and rendered several link requires accorduing to user navbar panel

---
+++ client/src/pages/user/UserHistory.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserNav from "../../component/nav/UserNav";
+
+const UserHistory = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-2">
+        <UserNav />
+      </div>
+      <div className="col">User history page</div>
+    </div>
+  </div>
+);
+
+export default UserHistory;
