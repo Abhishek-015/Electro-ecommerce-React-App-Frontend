6f05e9f098639f831bf4ab379fd0bcc500ea1e1e
Abhishek <abhishek404singh@gmail.com>
2022-01-13 09:48:05 +0530

    user token varified and svaing the user to the database

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,17 @@
-const admin = require('../firebase')
+const admin = require("../firebase");
 
-exports.authCheck=(req,res,next)=>{
-   console.log(req.headers)
-   next()
-}
+exports.authCheck = async (req, res, next) => {
+  // console.log(req.headers)
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    console.log("FIREBASEUSER IN AUTH CHECK", firebaseUser);
+    req.user = firebaseUser;
+  } catch (error) {
+    res.status(401).json({
+      err: "Invalid or Expired token",
+    });
+  }
+  next()
+};
