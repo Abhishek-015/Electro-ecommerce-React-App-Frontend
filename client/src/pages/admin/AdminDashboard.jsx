46f7dd70efa29f115f8defd5ab82ce01715c8d58
Abhishek <abhishek404singh@gmail.com>
2022-01-21 19:28:02 +0530

    create the end points at server side to show thr products on admin dashboard page

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNav from "../../component/nav/AdminNav";
+import { getProductsBYCount } from "../../utils/product";
 
-const AdminDashboard = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2">
-        <AdminNav />
-      </div>
-      <div className="col">
-          <h4>Admin Dashboard Page</h4>
+import {LoadingOutlined} from '@ant-design/icons'
+
+const AdminDashboard = () => {
+  const [products,setProducts] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    loadAllProducts()
+  },[])
+   
+  const loadAllProducts = () => {
+    setLoading(true)
+     getProductsBYCount(100)
+     .then(res =>{
+       setProducts(res.data)
+       setLoading(false)
+      })
+     .catch(err =>  {
+       setLoading(false)
+       console.log(err)
+      })
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div>
+        {loading ? (
+            <LoadingOutlined className="text-danger ml-5 h1"  />
+          ) : (
+            <h4 className="my-3">Products</h4>
+          )}
+          {JSON.stringify(products)}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AdminDashboard;
