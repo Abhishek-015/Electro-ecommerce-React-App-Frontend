5ed9e3c7e05263b7146231b0914a5e20b134e362
Abhishek Singh <abhishek404singh@gmail.com>
2022-03-16 16:59:24 +0530

    created coupon and remove coupon from fronted as well as backend completed

--- client/src/pages/admin/coupon/CreateCoupon.jsx
+++ client/src/pages/admin/coupon/CreateCoupon.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker"; // to get date table format
 import "react-datepicker/dist/react-datepicker.css";
 import { getCoupons, removeCoupon, createCoupon } from "../../../utils/coupon";
 import { DeleteOutlined } from "@ant-design/icons";
-import {useSelector} from "react-redux"
-import {toast} from "react-toastify"
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import AdminNav from "../../../component/nav/AdminNav";
 
 const CreateCouponPage = () => {
@@ -12,23 +12,47 @@ const CreateCouponPage = () => {
   const [expiry, setExpiry] = useState("");
   const [discount, setDiscount] = useState("");
   const [loading, setLoading] = useState(false);
+  const [coupons, setCoupons] = useState([]);
 
   //redux
-  const {user} = useSelector(state => ({...state}))
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadAllCoupons();
+  }, []);
+
+  const loadAllCoupons = () => getCoupons().then((res) => setCoupons(res.data));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true)
-    createCoupon({name,expiry,discount},user.token)
-    .then(res=>{
-      setLoading(false);
-      setName("");
-      setDiscount('');
-      setExpiry("")
-      toast.success(`"${res.data.name}" is created`)
-    }).catch(err=>{
-      setLoading(false)
-      console.log(err)})
+    setLoading(true);
+    createCoupon({ name, expiry, discount }, user.token)
+      .then((res) => {
+        setLoading(false);
+        loadAllCoupons(); //load all coupons
+        setName("");
+        setDiscount("");
+        setExpiry("");
+
+        toast.success(`"${res.data.name}" is created`);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
+  const handleRemove = (couponId) => {
+    if (window.confirm("Delete?")) {
+      setLoading(true);
+      removeCoupon(couponId, user.token)
+        .then((res) => {
+          loadAllCoupons(); //load all coupons
+          setLoading(false);
+          toast.success(`"${res.data.name}" deleted.`);
+        })
+        .catch((err) => console.log(err));
+    }
   };
   return (
     <div className="container-fluid">
@@ -37,7 +61,12 @@ const CreateCouponPage = () => {
           <AdminNav />
         </div>
         <div className="col-md-10">
-          <h4>Coupon</h4>
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Coupon</h4>
+          )}
+
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label className="text-muted">Name</label>
@@ -72,6 +101,33 @@ const CreateCouponPage = () => {
             </div>
             <button className="btn btn-primary btn-sm my-2">Save</button>
           </form>
+          <br />
+          <h4>{coupons.length}</h4>
+          <table className="table table-bordered">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Expiry</th>
+                <th scope="col">Discount</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coupons.map((coupon) => (
+                <tr key={coupon._id}>
+                  <td>{coupon.name}</td>
+                  <td>{new Date(coupon.expiry).toLocaleDateString()}</td>
+                  <td>{coupon.discount} %</td>
+                  <td>
+                    <DeleteOutlined
+                      className="text-danger pointer"
+                      onClick={() => handleRemove(coupon._id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
--- client/src/utils/coupon.js
+++ client/src/utils/coupon.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getCoupons = async () =>
   await axios.get(`${process.env.REACT_APP_API}/coupons`);
 
-export const removeCoupons = async (couponId, authToken) =>
+export const removeCoupon = async (couponId, authToken) =>
   await axios.delete(`${process.env.REACT_APP_API}/coupon/${couponId}`, {
     headers: {
       authToken,
