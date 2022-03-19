51a6aef0616ff193ab3e6595e31c93bc9e8765a0
Abhishek Singh <abhishek404singh@gmail.com>
2022-03-19 23:26:52 +0530

    created routes for wish list and also controller method to handle the endpoints

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,7 +12,10 @@ const {
   saveAddress,
   applyCouponToUserCart,
   createOrder,
-  orders
+  orders,
+  addToWishList,
+  wishlist,
+  removeFromWishlist,
 } = require("../controllers/user");
 
 //route
@@ -22,9 +25,15 @@ router.delete("/user/cart", authCheck, emptyCart); //empty cart
 router.post("/user/address", authCheck, saveAddress);
 
 //orders
-router.post('/user/order',authCheck,createOrder)
-router.get('/user/orders',authCheck,orders)
+router.post("/user/order", authCheck, createOrder);
+router.get("/user/orders", authCheck, orders);
 
 //coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
+
+//wishlist
+router.post("/user/wishlist", authCheck, addToWishList);
+router.get("/user/wishlist", authCheck, wishlist);
+router.put("/user/wishlist/:productId", authCheck, removeFromWishlist);
+
 module.exports = router;
